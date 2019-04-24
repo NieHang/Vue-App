3de5a2f7f1964327fba4a7bad1953fc0cc5c17b0
聂航 <1076883711@qq.com>
2019-04-24 15:58:26 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+// eslint-disable-next-line import/first
+import fastClick from 'fastclick';
+import './assets/styles/reset.css';
+import './assets/styles/border.css';
 
 Vue.config.productionTip = false;
+
+fastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
