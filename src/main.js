5347b4b274ec6e9882f4c669b81981432182ee33
聂航 <1076883711@qq.com>
2019-04-26 10:21:05 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/first */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
@@ -5,13 +7,17 @@ import App from './App';
 import router from './router';
 // eslint-disable-next-line import/first
 import fastClick from 'fastclick';
-import './assets/styles/reset.css';
-import './assets/styles/border.css';
-import './assets/styles/iconfont.css';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'styles/reset.css';
+import 'styles/border.css';
+import 'styles/iconfont.css';
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false;
 
 fastClick.attach(document.body);
+
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" :alt="item.id">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '1',
+          imgUrl:
+            'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/5daa78ddf9a3e3aca32195e079b9545d.jpg_750x200_3aeaa5c2.jpg'
+        },
+        {
+          id: '2',
+          imgUrl:
+            'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+// 修改子组件的样式
+.wrapper >>> .swiper-pagination-bullet-active {
+  background-color: #fff;
+}
+
+.wrapper {
+  width: 100%;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 26%;
+  background-color: #eee;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
