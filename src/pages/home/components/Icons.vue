9823d00f414da48c1da303087aebb521f5c7fa43
聂航 <1076883711@qq.com>
2019-04-26 15:11:53 +0800

    add swiper-icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img
+              class="icon-img-content"
+              :src="item.imgUrl"
+            >
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data() {
+    return {
+      iconList: [
+        {
+          id: '1',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '2',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/27/dac2bcf9added702.png',
+          desc: '海滨海岛'
+        },
+        {
+          id: '3',
+          imgUrl:
+            'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png',
+          desc: '自然风光'
+        },
+        {
+          id: '4',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        },
+        {
+          id: '5',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '威海必游'
+        },
+        {
+          id: '6',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/f5/a963333e1fa802.png',
+          desc: '当地好玩'
+        },
+        {
+          id: '7',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/48/cb72b99b71974c02.png',
+          desc: '休闲玩乐'
+        },
+        {
+          id: '8',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+          desc: '泡温泉'
+        },
+        {
+          id: '9',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+          desc: '亲子游'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages;
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+.icons >>> .swiper-container {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
+
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
+
+    .icon-img-content {
+      display: block;
+      margin: 0 auto;
+      height: 100%;
+    }
+  }
+
+  .icon-desc {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    color: $darkTextColor;
+    text-align: center;
+    ellipsis()
+  }
+}
+</style>
