6f1cdb0b0b048483139df5bb1cd30e5b96e66632
聂航 <1076883711@qq.com>
2019-04-30 12:24:33 +0800

    ajax city

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -12,51 +12,19 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div><div class="button-wrapper">
-            <div class="button">北京</div>
-          </div><div class="button-wrapper">
-            <div class="button">北京</div>
-          </div><div class="button-wrapper">
-            <div class="button">北京</div>
+          <div class="button-wrapper" v-for="item in hotCities" :key="item.id">
+            <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
+          <div
+            class="item border-bottom"
+            v-for="innerItem in item"
+            :key="innerItem.id"
+          >{{innerItem.name}}</div>
         </div>
       </div>
     </div>
@@ -68,49 +36,71 @@ import Bscroll from 'better-scroll'
 
 export default {
   name: 'CityList',
+  props: ['cities', 'hotCities'],
   mounted() {
     this.scroll = new Bscroll(this.$refs.wrapper)
-  },
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/varibles.styl'
-  .border-topbottom
-    &:before
-      border-color #ccc
-    &:after
-      border-color #ccc
-  .border-bottom
-    &:before
-      border-color #ccc
-  .list
-    overflow hidden
-    position absolute
-    top 1.58rem
-    left 0
-    right 0
-    bottom 0
-    .title
-      line-height .44rem
-      background-color #eee
-      padding-left .2rem
-      color #666666
-      font-size .26rem
-    .button-list
-      overflow hidden
-      padding .1rem
-      .button-wrapper
-        float left
-        width 33.33%
-        .button
-          text-align center
-          margin .1rem .6rem .1rem .1rem
-          padding .1rem 0
-          border-radius .06rem
-          border .02rem solid #ccc
-    .item-list
-      .item
-        line-height .76rem
-        padding-left .2rem
+@import '~styles/varibles.styl';
+
+.border-topbottom {
+  &:before {
+    border-color: #ccc;
+  }
+
+  &:after {
+    border-color: #ccc;
+  }
+}
+
+.border-bottom {
+  &:before {
+    border-color: #ccc;
+  }
+}
+
+.list {
+  overflow: hidden;
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  .title {
+    line-height: 0.44rem;
+    background-color: #eee;
+    padding-left: 0.2rem;
+    color: #666666;
+    font-size: 0.26rem;
+  }
+
+  .button-list {
+    overflow: hidden;
+    padding: 0.1rem;
+
+    .button-wrapper {
+      float: left;
+      width: 33.33%;
+
+      .button {
+        text-align: center;
+        margin: 0.1rem 0.6rem 0.1rem 0.1rem;
+        padding: 0.1rem 0;
+        border-radius: 0.06rem;
+        border: 0.02rem solid #ccc;
+      }
+    }
+  }
+
+  .item-list {
+    .item {
+      line-height: 0.76rem;
+      padding-left: 0.2rem;
+    }
+  }
+}
 </style>
