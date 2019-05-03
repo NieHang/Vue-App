64d88aa67653fd39e7e106f0b6c6add1fc697199
NieHang <1076883711@qq.com>
2019-05-03 10:52:48 +0800

    banner图，图片画廊效果完成

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,14 +2,20 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+      <router-link
+        tag="li"
+        :to="'/detail/' + item.id"
+        class="item border-bottom"
+        v-for="item in recommendList"
+        :key="item.id"
+      >
         <img :src="item.imgUrl" class="item-img">
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
-          <p class='item-desc'>{{item.desc}}</p>
+          <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
-      </li>
+      </router-link>
     </ul>
   </div>
 </template>
@@ -24,37 +30,51 @@ export default {
 </script>
 
 <style lang='stylus' scoped>
-  @import '~styles/mixins.styl'
-  .title
-    margin-top .2rem
-    line-height .8rem
-    background-color #eee
-    text-indent .2rem
-  .item
-    overflow hidden
-    display flex
-    height 1.9rem
-    .item-img
-      width 1.7rem
-      height 1.7rem
-      padding .1rem
-    .item-info
-      flex 1
-      padding .1rem
-      min-width 0
-      .item-title
-        line-height .54rem
-        font-size .32rem
-        ellipsis()
-      .item-desc
-        line-height .4rem
-        color #ccc
-        ellipsis()
-      .item-button
-        line-height .44rem
-        margin-top .16rem
-        background-color #ff9300
-        padding 0 .2rem
-        border-radius .06rem
-        color #fff
+@import '~styles/mixins.styl';
+
+.title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background-color: #eee;
+  text-indent: 0.2rem;
+}
+
+.item {
+  overflow: hidden;
+  display: flex;
+  height: 1.9rem;
+
+  .item-img {
+    width: 1.7rem;
+    height: 1.7rem;
+    padding: 0.1rem;
+  }
+
+  .item-info {
+    flex: 1;
+    padding: 0.1rem;
+    min-width: 0;
+
+    .item-title {
+      line-height: 0.54rem;
+      font-size: 0.32rem;
+      ellipsis();
+    }
+
+    .item-desc {
+      line-height: 0.4rem;
+      color: #ccc;
+      ellipsis();
+    }
+
+    .item-button {
+      line-height: 0.44rem;
+      margin-top: 0.16rem;
+      background-color: #ff9300;
+      padding: 0 0.2rem;
+      border-radius: 0.06rem;
+      color: #fff;
+    }
+  }
+}
 </style>
